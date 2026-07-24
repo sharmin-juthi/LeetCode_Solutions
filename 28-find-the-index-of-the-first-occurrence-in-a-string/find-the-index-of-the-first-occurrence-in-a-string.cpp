1d4e0c54440ea253c4a6d1459370665da1918ddd
Sharmin Akther <107158456+sharmin-juthi@users.noreply.github.com>
2026-07-24 11:30:44 +0600

    Time: 0 ms (100.00%) | Memory: 9.1 MB (45.20%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-      int len = needle.length();
-      int n = haystack.length();
-      for(int i=0;i<n;i++){
-        if(haystack.substr(i,len)==needle)
-          return i;
-        
-      }
+         int pos = haystack.find(needle);
 
-      return -1;
+         if(pos != string::npos)
+         return pos;
+         else
+         return -1;
     }
 };

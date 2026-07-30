cf3d39868d1bef6ab4cc4f4eaf44a8394eeb39e9
Sharmin Akther <107158456+sharmin-juthi@users.noreply.github.com>
2026-07-30 19:08:02 +0600

    Time: 49 ms (49.72%) | Memory: 29.1 MB (47.52%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -16,7 +16,9 @@ public:
                 if (sum == 0) {
                     res.push_back({nums[i], nums[left], nums[right]});
 
-                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[left] == nums[left + 1]) 
+                    left++;
+                    
                     while (left < right && nums[right] == nums[right - 1]) right--;
 
                     left++;
